ae8f606c8c70dae97af3ff5117018a1451227866
BalbinoEsen <balbino.aylagas@esen.edu.sv>
2021-05-21 19:06:48 -0600

    rest api added a resource

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from flask import Flask
 from flask_restful import Api
+from helloworld import HelloWorld
 
 app = Flask(__name__)
 api = Api(app)  # wrapper
+
+api.add_resource(HelloWorld, "/helloworld")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
